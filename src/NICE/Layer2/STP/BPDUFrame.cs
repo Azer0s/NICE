a251b68bb2d3d6072b46890790d425cfa12e9243
Ariel Simulevski <ariel@simulevski.at>
2019-05-23 18:07:41 +0200

    Update BPDUFrame.cs

--- src/NICE/Layer2/STP/BPDUFrame.cs
+++ src/NICE/Layer2/STP/BPDUFrame.cs
@@ -1,6 +1,9 @@
 // ReSharper disable InconsistentNaming
 namespace NICE.Layer2.STP
 {
+    /// <summary>
+    /// https://ccnaeducation.com/802-1d-bpdu-frame-format/
+    /// </summary>
     public class BPDUFrame
     {
         public byte[] ToBytes()
